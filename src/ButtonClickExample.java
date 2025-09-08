706e36ee5ba7ccfe52edef7ce892dfcd791ae3ee
paulgries <pgries@cs.toronto.edu>
2025-09-08 14:30:19 -0400

    Swing brief overview

--- src/ButtonClickExample.java
+++ src/ButtonClickExample.java
@@ -30,6 +30,14 @@ public class ButtonClickExample {
                 }
             });
 
+            cancel.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    firstNameField.setText("");
+                    lastNameField.setText("");
+                }
+            });
+
             JPanel mainPanel = new JPanel();
             mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
             mainPanel.add(firstNamePanel);
---
+++ src/MainFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainFrame {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Intro JFrame Example");
+            frame.setMinimumSize(new java.awt.Dimension(300, 200));
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        });
+    }
+}
